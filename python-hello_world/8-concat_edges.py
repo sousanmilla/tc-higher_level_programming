3a08a0ec0104369e898c8d296f0e03330d0ed3a1
sousanmilla <sousa.nunes.milla@gmail.com>
2025-11-25 11:09:18 +0000

    ajustes

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[38:66], str[107:111], str[0:6])
+print(str[38:66]+str[106:112]+str[0:6])
