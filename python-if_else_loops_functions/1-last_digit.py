2b86514d7b079e70edd80e1ccb42636ebc6f69be
sousanmilla <sousa.nunes.milla@gmail.com>
2025-11-27 18:36:16 +0000

    praticando if e else com int

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if int(str(number)[-1]) > 5:
+    print("Last digit of", number, "is", end=" ")
+    print(int(str(number)[-1]), "and is greater than 5")
+elif int(str(number)[-1]) == 0:
+    print("Last digit of", number, "is", int(str(number)[-1]), "and is 0")
+else:
+    print("Last digit of", number, "is", end=" ")
+    print(int(str(number)[-1]), "and is less than 6 and not 0")
