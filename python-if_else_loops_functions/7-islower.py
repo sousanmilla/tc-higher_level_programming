72de27bee98cb5a8991bd0945f97f5be2d390c7c
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-04 23:29:35 +0000

    verificando se a letra e maiuscula ou minuscula

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -7,5 +7,5 @@ def islower(c):
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
+print("c is {}".format("lower" if islower("c") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
