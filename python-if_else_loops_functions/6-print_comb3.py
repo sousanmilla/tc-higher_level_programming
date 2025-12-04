56b418a753cc3be2a38d2def857a7a2ddcc550b3
sousnmilla <sousa.nunes.milla@gmail.com>
2025-12-04 19:05:15 +0000

    imprimindo diferentes combinacoes de numeros

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(1, 50):
+    if i > 9 and i < 12 or i > 19 and i < 23 or i > 39 and i < 45:
+        continue
+    print("{:02d}, ".format(i), end="")
+for i in range(56, 79):
+    if i > 59 and i < 67 or i > 69 and i < 78:
+        continue
+    print("{:02d}, ".format(i), end="")
+print(89)
