cdc65ffd563966e188918f2b12100f83db1ed12c
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-05 11:10:07 +0000

    imprimindo texto em capslock

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -8,5 +8,4 @@ def uppercase(str):
         else:
             result += char
     return result
-print("{}".format(uppercase("best")))
-print("{}".format(uppercase("Best School 98 Battery street")))
+print("{}".format(uppercase("holberton")))
