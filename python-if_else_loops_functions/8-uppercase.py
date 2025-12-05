5a701687c68417e463928076312a56fdd11c6f42
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-05 00:48:33 +0000

    deixando em capslock

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        char_code = ord(char)
+        if char_code >= 97 and char_code <= 122:
+            result += chr(char_code - 32)
+        else:
+            result += char
+    return result
+    print(uppercase("best"))
+    print(uppercase("Best School 98 Battery street"))
