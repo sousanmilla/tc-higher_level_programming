7d857b955cfe8c559e7f1609e222dcc11b8b37c5
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-12 17:50:58 +0000

    usando import para fazer uma calculadora simples

---
+++ python-import_modules/calculation_1.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    soma = a + b
+    return soma
+
+
+def sub(a, b):
+    subtracao = a - b
+    return subtracao
+
+
+def mul(a, b):
+    multi = a * b
+    return multi
+
+
+def div(a, b):
+    divisao = a / b
+    return divisao
