9d761f3b7194aae89683b87e844450d131f715a4
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-16 16:46:42 +0000

    soma infinita de argumentos

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,4 +3,13 @@ import sys
 
 
 def main():
-    
+
+    args = sys.argv[1:]
+    total = 0
+    for valor in args:
+        total += int(valor)
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
