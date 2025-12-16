033311871ce555cfd7ad1f64df892e6afb00a137
sousanmilla <sousa.nunes.milla@gmail.com>
2025-12-16 14:41:04 +0000

    imprimindo o numero e a lista de argumentos

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+
+    n = len(sys.argv) - 1
+    args = sys.argv[1:]
+    if n == 1:
+        print("{} argument:".format(n))
+    elif n == 0:
+        print("{} arguments.".format(n))
+    else:
+        print("{} arguments:".format(n))
+
+    if n > 0:
+        for i in range(len(args)):
+            print("{}: {}".format(i + 1, args[i]))
+
+
+if __name__ == "__main__":
+    main()
