9cf6c5b85ac252dbae5a9a7ec76b01c9264af6ef
sousanmilla <sousa.nunes.milla@gmail.com>
2026-01-07 19:34:30 +0000

    colocando em ordem decrescente

---
+++ python-data_structures/3-print-reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    my_list.sort(reverse=True)
+    for i in my_list:
+        print("{:d}".format(i))
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_reversed_list_integer(my_list)
