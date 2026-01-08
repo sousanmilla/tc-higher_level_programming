92a0222c89a21f3b421e827c87544318b04f0aff
sousanmilla <sousa.nunes.milla@gmail.com>
2026-01-08 18:44:04 +0000

    verificando se e divisivel por 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,11 +3,8 @@
 def divisible_by_2(my_list=[]):
     list_result = []
     for i in my_list:
-        if my_list:
-            list_result.append(i % 2 == 0)
-            return list_result
-        else:
-            return None
+        list_result.append(i % 2 == 0)
+    return list_result
 
 
 if __name__ == "__main__":
@@ -16,6 +13,9 @@ if __name__ == "__main__":
 
     i = 0
     while i < len(list_result):
-        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else
-    "is not"))
+        print("{:d} {:s} divisible by 2".format(
+            my_list[i],
+            "is" if list_result[i] else
+            "is not"
+        ))
         i += 1
