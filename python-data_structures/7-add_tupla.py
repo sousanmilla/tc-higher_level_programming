dbcb88543f30db7969b4e82a23d4100ba02cc97e
sousanmilla <sousa.nunes.milla@gmail.com>
2026-01-08 14:52:38 +0000

    somando tuplas

---
+++ python-data_structures/7-add_tupla.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
