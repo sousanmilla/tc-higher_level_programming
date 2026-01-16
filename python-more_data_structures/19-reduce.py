a09ec22698343a9ec5ff2bf58ecf2c68cfb311ac
sousanmilla <sousa.nunes.milla@gmail.com>
2026-01-16 04:12:56 +0000

    achando a mediada dos valores de um dicionario

---
+++ python-more_data_structures/19-reduce.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from functools import reduce
+
+
+def calc_average(a_dictionary):
+    n = len(a_dictionary)
+    if n == 0:
+        return 0.0, 0.0
+
+    meds, medi = reduce(
+        lambda acc, r: (acc[0] + r['salary'], acc[1] + r['age']),
+        a_dictionary,
+        (0.0, 0.0)
+        )
+    return meds / n, medi / n
+
+
+if __name__ == "__main__":
+    data = [
+        {'name': 'Alice', 'age': 50, 'salary': 5000},
+        {'name': 'Bob', 'age': 30, 'salary': 7000},
+        {'name': 'Charlie', 'age': 35, 'salary': 9000},
+        {'name': 'Dave', 'age': 40, 'salary': 11000},
+    ]
+
+    average_salary, average_age = calc_average(data)
+
+    print(
+        f'The average salary is R${average_salary:.2f} '
+        f'with an average age of {average_age}'
+    )
