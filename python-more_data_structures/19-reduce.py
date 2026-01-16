baf8387114ff5993b187f1d421eecfca8c0399d1
sousanmilla <sousa.nunes.milla@gmail.com>
2026-01-16 13:06:42 +0000

    usando reduce para achar a medida dos valores em um dicionario

--- python-more_data_structures/19-reduce.py
+++ python-more_data_structures/19-reduce.py
@@ -6,8 +6,8 @@ from functools import reduce
 def calc_average(a_dictionary):
     salaries = [item['salary'] for item in a_dictionary]
     ages = [item['age'] for item in a_dictionary]
-    somas = reduce(lambda x, y: x + y, salaries)
-    somaa = reduce(lambda x, y: x + y, ages)
+    somas = reduce(lambda x, y: x + y, salaries, 0)
+    somaa = reduce(lambda x, y: x + y, ages, 0)
     n = len(a_dictionary)
     return somas / n, somaa / n
 
