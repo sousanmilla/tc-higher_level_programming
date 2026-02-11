373a5cf365d96d6d48a4fc48e0b8c14a94a7ce02
sousanmilla <sousa.nunes.milla@gamil.com>
2026-02-11 14:41:00 +0000

    dividindo uma lista

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    result = None
-    try:
-        result = my_list_1 / my_list_2
-    except TypeError:
-        return print("wrong type")
-    except ZeroDivisionError:
-        return print("division by 0")
-    except:
-        if my_list_1 < my_list_2 or my_list_2 < my_list_1:
+    result = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            res = a / b
+        except TypeError:
+            print("wrong type")
+            res = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            res = 0
+        except IndexError:
             print("out of range")
-    finally:
-        print()
+            res = 0
+        finally:
+            result.append(res)
     return result
 
 
