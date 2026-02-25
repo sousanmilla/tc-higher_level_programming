6fcfcc18c1398aa5c85bda9246e520f16e990e4f
sousanmilla <sousa.nunes.milla@gmail.com>
2026-02-25 00:28:41 +0000

    funcao de soma com testes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,3 +18,26 @@ def add_integer(a, b=98):
     a = int(a)
     b = int(b)
     return a + b
+
+
+assert add_integer(1, 2) == 3
+assert add_integer(100, -2) == 98
+assert add_integer(2) == 100
+assert add_integer(100.3, -2) == 98
+assert add_integer(0, 0) == 0
+assert add_integer(-5) == 93
+
+
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+    print(add_integer(100, -2))
+    print(add_integer(2))
+    print(add_integer(100.3, -2))
+    try:
+        print(add_integer(4, "School"))
+    except Exception as e:
+        print(e)
+    try:
+        print(add_integer(None))
+    except Exception as e:
+        print(e)
