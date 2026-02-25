53fecce5469b2f8d123ca1bb8aa8123eae3b28ea
sousanmilla <sousa.nunes.milla@gmail.com>
2026-02-25 17:29:06 +0000

    funca para divisao de matriz por um numero

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,32 @@
+#!/usr/bin/python3
+"""
+Esse módulo contem uma função para divisão de um amatriz por um número inteiro.
+"""
 
+
+def matrix_divided(matrix, div):
+    """Conferindo que a matrix só contem números."""
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)")
+    """Conferindo que as linhas da matrix tem o mesmo tamanho."""
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+    """Conferindo que div é um número."""
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    """Conferindo que div não é zero."""
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = [[round((i / div), 2) for i in r] for r in matrix]
+    return new_matrix
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ]
+    print(matrix_divided(matrix, 3))
+    print(matrix)
