ca0773603db4972eddc78d2c88f8dfbb4866832a
sousanmilla <sousa.nunes.milla@gmail.com>
2026-02-25 20:45:36 +0000

    funcao para reproduzir o nome e sobrenome

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name=""):
     """Conferindo se o sobrenome é string."""
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}$".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
 
 
 if __name__ == "__main__":
