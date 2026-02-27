379794dd5dce3cc2313ab71276d1ff1edc354154
sousanmilla <sousa.nunes.milla@gmail.com>
2026-02-27 15:27:08 +0000

    usando unittetst para testar funcao de max_integer

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_normal(self)
+        self.assert
