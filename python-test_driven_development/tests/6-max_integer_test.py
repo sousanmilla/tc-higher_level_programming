ba5780c856d36cb2370a153d1d49be1c56ab0316
sousanmilla <sousa.nunes.milla@gmail.com>
2026-02-27 17:29:57 +0000

    usando unittetst para testar funcao de max_integer

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -23,24 +23,23 @@ if __name__ == "__main__":
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_max_no_fim(self):
+    def test_max_at_end(self):
         self.assertTrue(max_integer([1, 2, 3, 4]) == 4)
 
-    def test_max_no_meio(self):
+    def test_max_in_middle(self):
         self.assertTrue(max_integer([1, 3, 4, 2]) == 4)
 
-    def test_max_no_inicio(self):
+    def test_max_at_beginning(self):
         self.assertTrue(max_integer([4, 3, 1, 2]) == 4)
 
-    def test_max_tudo_igual(self):
-        self.assertTrue(max_integer([4, 4, 4, 4]) == 4)
+    def test_one_negative_number(self):
+        self.assertTrue(max_integer([1, -3, 2, 4]) == 4)
 
-
-    def test_max_tudo_negativo(self):
+    def test_only_negative_numbers(self):
         self.assertTrue(max_integer([-1, -3, -4, -2]) == -1)
 
-    def test_max_so_um(self):
+    def test_one_element(self):
         self.assertTrue(max_integer([4]) == 4)
 
-    def test_vazio(self):
+    def test_list_is_empty(self):
         self.assertTrue(max_integer() is None)
